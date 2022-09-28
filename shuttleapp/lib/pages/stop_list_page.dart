83b7e189262ea754e3d78b766d7bdc867fad5847
aloysiusyap <yap_js@yahoo.com>
2022-09-29 02:26:46 +0800

    Added StopCard widget

--- shuttleapp/lib/pages/stop_list_page.dart
+++ shuttleapp/lib/pages/stop_list_page.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:shuttleapp/model/route.dart' as Route;
+import 'package:shuttleapp/widget/stopCard.dart';
 
 class StopListPage extends StatelessWidget {
   const StopListPage({Key? key, required this.route}) : super(key:key);
 
   final Route.Route route;
-  
+
+
   @override
   Widget build(BuildContext context) {
+
+    String availableDays = frequencyToAvailableDays(route.frequency);
+
     return Scaffold(
 
       appBar: AppBar(
-        toolbarHeight: 400,
+        toolbarHeight: 200,
         backgroundColor: Colors.blue,
       ),
       body: Column(
         children: [
           Container(), // Text Field Goes Here (Start, Stop, Days per week, Price
-          
+
           Expanded(
             child: ListView.builder(itemCount: route.stops.length, itemBuilder: (context, index) {
-              return ListTile(); // StopCard Goes Here
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+                child: Card(
+                  color: const Color.fromRGBO(249, 244, 232, 1),
+                  child: StopCard(route: route, index: index, availableDays: availableDays,),
+                ),
+              );
             },)
           )
             
@@ -30,3 +41,20 @@ class StopListPage extends StatelessWidget {
     );
   }
 }
+
+
+String frequencyToAvailableDays(List<bool> frequency) {
+  var listOfDays = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+  String result = "";
+
+  for (var i = 0; i < frequency.length; i++) {
+    if (frequency.elementAt(i) == true) {
+      result += '${listOfDays.elementAt(i)} ,';
+    }
+  }
+
+  return result.substring(0, result.length-2);
+}
+
+
